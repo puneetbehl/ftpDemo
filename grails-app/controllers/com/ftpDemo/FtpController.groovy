f205cf5651a78d7191a041ae436fd5be55f34c54
Puneet <puneet.behl@intelligrape.com>
2014-07-19 01:18:04 +0530

    Updated password for Ftp.

--- grails-app/controllers/com/ftpDemo/FtpController.groovy
+++ grails-app/controllers/com/ftpDemo/FtpController.groovy
@@ -10,7 +10,7 @@ class FtpController {
     def upload() {
         File file = File.createTempFile("temp", "txt")
         file.text = "Sample File\n"
-        FtpCredential ftpCredential = new FtpCredential(server: 'frs.sourceforge.net', username: 'behl', password: 'test123', port: 22, remoteBaseDir: '/home/frs/project/sampleftpdemo')
+        FtpCredential ftpCredential = new FtpCredential(server: 'frs.sourceforge.net', username: 'behl', password: 'test@12345', port: 22, remoteBaseDir: '/home/frs/project/sampleftpdemo')
         InputStream inputStream = new BufferedInputStream(new FileInputStream(file))
         ftpService.save(inputStream, "sampleFile.txt", ftpCredential)
 
